c297d4ded87b1db38c78aab703272e132a1b63ff
相北 <xiangbei.yzx@alibaba-inc.com>
2022-01-25 22:12:48 +0800

    QuickStart add insert data

--- case-clickhouse/src/main/java/com/yuanzhixiang/clickhouse/ru/yandex/QuickStart.java
+++ case-clickhouse/src/main/java/com/yuanzhixiang/clickhouse/ru/yandex/QuickStart.java
@@ -1,5 +1,8 @@
 package com.yuanzhixiang.clickhouse.ru.yandex;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+
 /**
  * @author ZhiXiang Yuan
  * @date 2022/01/25 10:59:29
@@ -8,5 +11,7 @@ public class QuickStart {
 
     public static void main(String[] args) throws Exception {
         Class.forName("com.clickhouse.jdbc.ClickHouseDriver");
+        // http 端口 8123
+        Connection connection = DriverManager.getConnection("jdbc:clickhouse://127.0.0.1:9000");
     }
 }
--- case-clickhouse/src/main/resources/clickhouse.sql
+++ case-clickhouse/src/main/resources/clickhouse.sql
@@ -1,2 +1,19 @@
 CREATE DATABASE IF NOT EXISTS tutorial;
 
+DROP TABLE tutorial.quant_stock_minute5_quotation;
+
+CREATE TABLE IF NOT EXISTS tutorial.quant_stock_minute5_quotation
+(
+    symbol     String,
+    date       DateTime64(3),
+    adjustment Decimal(8, 4),
+    open       Decimal(6, 2),
+    close      Decimal(6, 2),
+    low        Decimal(6, 2),
+    high       Decimal(6, 2),
+    volume     UInt32
+)
+    engine = MergeTree()
+    ORDER BY  (date)
+
+
