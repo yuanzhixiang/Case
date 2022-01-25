de1814f8340522f1ac92cd6717086847741307cd
相北 <xiangbei.yzx@alibaba-inc.com>
2022-01-25 23:20:11 +0800

    Add mybatis plus operator clickhouse

---
+++ case-clickhouse/src/main/java/com/yuanzhixiang/clickhouse/mybatisplus/mapper/QuantStockMinute5QuotationMapper.java
@@ -0,0 +1,16 @@
+package com.yuanzhixiang.clickhouse.mybatisplus.mapper;
+
+
+import org.apache.ibatis.annotations.Mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.yuanzhixiang.clickhouse.mybatisplus.po.QuantStockMinute5Quotation;
+
+/**
+ * @author ZhiXiang Yuan
+ * @date 2022/01/25 22:45:37
+ */
+@Mapper
+public interface QuantStockMinute5QuotationMapper extends BaseMapper<QuantStockMinute5Quotation> {
+
+}
